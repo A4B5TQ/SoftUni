9b06bcac7f0f9b13fdbe2a0dc54610c4ed6b834a
Ivan Ivanov <ivanov.ivan_@abv.bg>
2016-03-19 22:47:05 +0200

    TestChanges

--- JavaFundamental/JavaSyntax/src/com/company/FormattingNumbers.java
+++ JavaFundamental/JavaSyntax/src/com/company/FormattingNumbers.java
@@ -6,21 +6,20 @@ import java.util.Scanner;
 public class FormattingNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        Locale.setDefault(Locale.ROOT);
-
+        sc.useLocale(Locale.ROOT);
         int a = sc.nextInt();
         double b = sc.nextDouble();
         double c = sc.nextDouble();
 
-        System.out.printf("|%1$-10x|%4$10s|%2$10.2f|%3$-10.3f",a,b,c,toBinaryPadLeft(a));
+        System.out.printf("|%1$-10x|%4$10s|%2$10.2f|%3$-10.3f", a, b, c, toBinaryPadLeft(a));
     }
-    static String toBinaryPadLeft(int a){
+
+    static String toBinaryPadLeft(int a) {
 
         StringBuilder sb = new StringBuilder();
         String binaryNumber = Integer.toBinaryString(a);
         for (int i = 0; i < 10 - binaryNumber.length(); i++) {
-             sb.append(0);
+            sb.append(0);
         }
         sb.append(binaryNumber);
         return sb.toString();
