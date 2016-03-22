116aabec7d80ae36af2c5da61c8b5b3dbc8bf08d
Ivan Ivanov <ivanov.ivan_@abv.bg>
2016-03-22 21:17:51 +0200

    Homework

--- JavaFundamental/JavaStreams/src/ALLCAPITALS.java
+++ JavaFundamental/JavaStreams/src/ALLCAPITALS.java
@@ -1,22 +1,26 @@
 import java.io.*;
 
-public class SumLines {
+public class ALLCAPITALS {
     static final File FILE_PATH = new File("res/lines.txt");
+
     public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                long charSum = 0;
-                for (int i = 0; i < line.length(); i++) {
-                    charSum += line.charAt(i);
-                }
-                System.out.println(charSum);
+                sb.append(line.toUpperCase() + "\r\n");
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
 
+        try (PrintWriter writer = new PrintWriter(new FileWriter(FILE_PATH))) {
+            writer.write(sb.toString());
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
