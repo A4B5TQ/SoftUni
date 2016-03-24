e5bc8cd0867ad3f616b4e65667d159a6aa714a6d
Ivan Ivanov <ivanov.ivan_@abv.bg>
2016-03-24 21:50:41 +0200

    Homework

---
+++ JavaFundamental/JavaCollections/src/SortArrayOfNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortArrayOfNumbers {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = Integer.parseInt(sc.nextLine());
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        for (Integer num : arr) {
+            System.out.printf("%d ",num);
+        }
+    }
+}
