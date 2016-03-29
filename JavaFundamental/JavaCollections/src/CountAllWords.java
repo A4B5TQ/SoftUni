a5a4963f0790dbba4745e378b871f4d52301d60b
Ivan Ivanov <ivanov.ivan_@abv.bg>
2016-03-29 18:53:57 +0300

    Simple changes

--- JavaFundamental/JavaCollections/src/CountAllWords.java
+++ JavaFundamental/JavaCollections/src/CountAllWords.java
@@ -1,10 +1,19 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CountAllWords {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String[] words = sc.nextLine().split("\\W+");
-        System.out.println(words.length);
+        String words = sc.nextLine();
+        Pattern pat = Pattern.compile("[\\p{L}]+");
+        Matcher match = pat.matcher(words);
+
+        long counter = 0;
+        while (match.find()) {
+            counter++;
+        }
+        System.out.println(counter);
     }
 }
